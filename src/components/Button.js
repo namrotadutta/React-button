2fbafc85021de6ff2aacb0c96f3a2365a2bd49f5
Bhavesh Sharma <34792643+iBhavesh@users.noreply.github.com>
2021-05-14 22:25:05 +0530

    Fix button click logic

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,40 +1,36 @@
-import React from 'react'
+import React from "react";
 
+class Button extends React.Component {
+  constructor(props) {
+    super(props);
 
-
-class Button extends React.Component{
-    constructor(props){
-        super(props);
-
-        this.state={
-            isButtonClicked :false
-        }
-    }
-
-    handleClick(){
-        if(!this.state.isButtonClicked){
-            this.setState({
-                isButtonClicked:true
-            }) 
-        }
-    }
-   
-
-
-    render(){
-    if(this.state.isButtonClicked) {
-        const buttonClass = 'Boton-green';
-    }else {
-        const buttonClass = 'Boton';
-    }
-        return(
-            <div>
-                
-                <button className={buttonClass} onClick={()=> this.handleClick()}>Press</button>
-                {this.state.isButtonClicked && <p>Button Clicked</p>}
-            </div>
-        )
+    this.state = {
+      isButtonClicked: false,
     };
+  }
+
+  handleClick() {
+    if (!this.state.isButtonClicked) {
+      this.setState({
+        isButtonClicked: true,
+      });
+    }
+  }
+
+  render() {
+    let buttonClass = "Boton";
+    if (this.state.isButtonClicked) {
+      buttonClass = "Boton-green";
+    }
+    return (
+      <div>
+        <button className={buttonClass} onClick={() => this.handleClick()}>
+          Press
+        </button>
+        {this.state.isButtonClicked && <p>Button Clicked</p>}
+      </div>
+    );
+  }
 }
 
-export default Button ;
+export default Button;
